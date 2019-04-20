2d6dd70953003f82345a62bffc2d5b007f752341
Alexius Academia <alexius.academia@gmail.com>
2019-04-20 22:30:06 +0800

    Added some components

---
+++ src/components/Home/index.jsx
@@ -0,0 +1,21 @@
+import React, {Component} from 'react'
+import './index.css'
+import { Jumbotron, Row, Col, Button , Container} from 'react-bootstrap'
+
+class Home extends Component {
+    render() {
+        return (
+            <Container>
+                <Jumbotron>
+                    <h2>SyncSoft Solutions</h2>
+                    <p>
+                        Your solution for custom softwares and systems from small scales 
+                        like mobile apps and simple websites to larger systems like employee systems.
+                    </p>    
+                </Jumbotron>
+            </Container>
+        )
+    }
+}
+
+export default Home
