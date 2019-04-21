5df75d8e5edb6ca0802a8553e4200d898441bbe3
Alexius Academia <alexius.academia@gmail.com>
2019-04-21 11:14:36 +0800

    Updated jumbotron of news page.

--- src/components/News/index.jsx
+++ src/components/News/index.jsx
@@ -1,11 +1,17 @@
 import React, {Component} from 'react'
+import { Container, Jumbotron } from 'react-bootstrap'
 
 export default class News extends Component {
     render() {
         return (
-            <div>
-                News
-            </div>
+            <Container>
+                <Jumbotron>
+                    <h2>News</h2>
+                    <p>
+                        Get our latest updates and industry solutions.
+                    </p>    
+                </Jumbotron>
+            </Container>
         )
     }
 }
