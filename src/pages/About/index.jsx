a1b9533be7e0dff1c2587e58168ffa03cd2a4ea0
Alexius Academia <alexius.academia@gmail.com>
2019-04-21 11:26:24 +0800

    Completed pages files.

---
+++ src/pages/About/index.jsx
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+import { Container, Jumbotron } from 'react-bootstrap'
+
+export default class About extends Component {
+    render() {
+        return (
+            <Container>
+                <Jumbotron>
+                    <h2>About Us</h2>
+                    <p>
+                    </p>    
+                </Jumbotron>
+            </Container>
+        )
+    }
+}
