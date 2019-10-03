966f300f358a8c44ddfdc4c3e0d3d6e7e041ba94
Jan Alexis Monsalud <engr.leir@gmail.com>
2019-10-03 17:13:38 +0800

    added js animations

---
+++ js/main.js
@@ -0,0 +1,53 @@
+/** Smooth scroll links */
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    const sectionElement = document.querySelector(this.getAttribute('href'))
+    const yCoordinate = sectionElement.getBoundingClientRect().top + window.pageYOffset;
+    const yOffset = -68;
+
+    window.scrollTo({
+      top: yCoordinate + yOffset,
+      behavior: 'smooth'
+    });
+  });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+  // Check if there are any navbar burgers
+  if ($navbarBurgers.length > 0) {
+
+    // Add a click event on each of them
+    $navbarBurgers.forEach(el => {
+      el.addEventListener('click', () => {
+
+        // Get the target from the "data-target" attribute
+        const target = el.dataset.target;
+        const $target = document.getElementById(target);
+
+        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+        el.classList.toggle('is-active');
+        $target.classList.toggle('is-active');
+
+      });
+    });
+  }
+
+});
+
+/**  hide/show navbar when scrolling */
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function () {
+//   var currentScrollPos = window.pageYOffset;
+//   if (prevScrollpos > currentScrollPos) {
+//     document.getElementById("navbar").style.top = "0";
+//   } else {
+//     document.getElementById("navbar").style.top = "-4.25rem";
+//   }
+//   prevScrollpos = currentScrollPos;
+// }
